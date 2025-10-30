9f24d3e14e3f9a02bc3f58e3becbc3ba87fa1f1b
belkisarslan <belkisarslan33@gmail.com>
2025-10-30 12:22:24 +0300

    star added

--- src/components/Characters/Characters.vue
+++ src/components/Characters/Characters.vue
@@ -3,11 +3,14 @@
     <div :class="styles.characterSection">
       <h3 :class="styles.title">Rick And Morty Karakterleri</h3>
       <div :class="styles.characters">
-        <div :class="styles.character" v-for="(c,i) in characters" :key="c.id">
+        <div :class="styles.character" v-for="(c, i) in characters" :key="i">
           <img :src="c.image" :alt="c.name" />
           <div :class="styles.characterOverlay">
             <div :class="styles.characterName">{{ c.name }}</div>
             <div :class="styles.characterInfo">{{ c.species }} — {{ c.status }}</div>
+            <img v-if="staredCharacter.includes(i)" src="../../assets/stared.svg" alt="" @click="starCharacter(i)">
+            <img v-else src="../../assets/star.svg" alt="" @click="starCharacter(i)">
+
           </div>
         </div>
       </div>
@@ -16,25 +19,47 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import styles from './styles.module.css'
-import { onMounted, computed } from 'vue'
-import { useStore } from 'vuex'
 
 export default {
-  setup() {
-    const store = useStore()
-
-    const characters = computed(() => store.getters['characters/allCharacters'])
-
-    onMounted(() => {
-      store.dispatch('characters/fetchCharacters')
-    })
-
+  data() {
     return {
-      characters,
-      styles: styles,
+      styles,
+      staredCharacter: [],
     }
-  }
+  },
+
+  computed: {
+
+    ...mapGetters('characters', ['allCharacters']),
+    characters() {
+      return this.allCharacters
+    },
+  },
+
+  methods: {
+
+    ...mapActions('characters', ['fetchCharacters']),
+
+    starCharacter(i) {
+      const index = this.staredCharacter.indexOf(i)
+
+      if (index === -1) {
+        // Eğer dizide yoksa, ekle
+        this.staredCharacter.push(i)
+      } else {
+        // Eğer zaten varsa, diziden çıkar
+        this.staredCharacter.splice(index, 1)
+      }
+    }
+  },
+
+  mounted() {
+
+    this.fetchCharacters()
+  },
+
+
 }
 </script>
-
